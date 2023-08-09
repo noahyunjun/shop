f6490e5ceaf3c4a28b32f60b586fc51979b4932d
HamHyeonJun <hyunjun003@naver.com>
2023-08-09 18:39:49 +0900

    **

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ root.render(
     </Provider>
   </QueryClientProvider>
 );
-
+//test code
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
